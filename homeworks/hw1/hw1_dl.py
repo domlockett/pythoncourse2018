9308b22470bf82bee295579856d4df206efd65a5
domlockett <34702780+domlockett@users.noreply.github.com>
2018-08-09 00:23:57 -0500

    buy stock functioning

--- homeworks/hw1/hw1_dl.py
+++ homeworks/hw1/hw1_dl.py
@@ -21,24 +21,29 @@ class MutualFund(object):
 class Portfolio(object):
     def __init__(self):
         self.cash = 0
-        hist= [('Account created: %d-%d-%d %d:%d - $ 0' % (now.year, now.month, now.day, now.hour, now.minute))]
+        self.hist= [('Account created: %d-%d-%d %d:%d - $ 0' % (now.year, now.month, now.day, now.hour, now.minute))]
         #port = {"Stock" : {}, "Mutual_Fund" : {}}
 
     ## Add cash to the portfolio
     def addCash(self,amount):
         self.cash += float(amount)
-        hist.append('Deposited $%.f: %d-%d-%d %d:%d - $ %.2f' % (now.year, now.month, now.day, now.hour, now.minute, self.cash) )
+        self.hist.append('Deposited $%.f: %d-%d-%d %d:%d - $ %.2f' % (amount, now.year, now.month, now.day, now.hour, now.minute, self.cash) )
         return self.cash
     
     ## Buys stock with the number of shares and the particular stock 
     def buyStock(self,shares, stock):
-        self.cash -= (float(amount)*stock.price)
-        hist.append = ('Purchased %.2f of %s: %d-%d-%d %d:%d - $ %.2f' % (shares, stock, now.year, now.month, now.day, now.hour, now.minute,self.cash) )
+        if float(shares * stock.price) > self.cash:
+            print "Transaction error: Insufficient funds"
+        else:
+            self.cash -= (shares*stock.price)
+            self.hist.append ('Purchased %.2f of %s: %d-%d-%d %d:%d - $ %.2f' % (shares, stock, now.year, now.month, now.day, now.hour, now.minute,self.cash) )
         return self.cash
     
 
     ## Buy mutual funds (1$ a piece)
     def buyMutualFunds(self, shares):
+        if float(shares ) > self.cash:
+            print "Transaction error: Insufficient funds"
         self.cash -= float(shares)
         hist.append = ('Purchased %.2f of %s: %d-%d-%d %d:%d - $ %.2f' % (now.year, now.month, now.day, now.hour, now.minute, self.cash) )
 
