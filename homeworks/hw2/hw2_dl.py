7bf106c99cd4b7d01fb7c58f563f7eadbde99bd6
domlockett <34702780+domlockett@users.noreply.github.com>
2018-08-13 23:15:56 -0500

    began hw 1

---
+++ homeworks/hw2/hw2_dl.py
@@ -0,0 +1,27 @@
+##• Go to https://petitions.whitehouse.gov/petitions 
+##• Go to the petition page for each of the petitions. 
+##• Create a .csv file with the following information for each petition:
+##   – Title # h3
+##   – Published date # h4 class 'petition-attribution'
+##   – Issues #div class = 'field field-name-field-petition-issues field-type-taxonomy-term-reference field-label-hidden tags'
+from bs4 import BeautifulSoup
+import urllib2 
+import random
+import time
+import os
+import re
+
+with open('hw2_dl.csv', 'wb') as f:
+	w = csv.DictWriter(f, fieldnames = ("Title", "Published date", "Issues"))
+	w.writeheader()
+
+web_address='https://petitions.whitehouse.gov/petitions '
+web_page = urllib2.urlopen(web_address)
+
+all_html = BeautifulSoup(web_page.read())
+all_topics = all_html.find_all("h3")[3:]
+all_topics = [i.text for i in all_topics]
+topic = []
+for i in all_topics:
+    topic.append(i.encode('utf-8', 'ignore'))
+
