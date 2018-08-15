26f5ecd15ccc4e614fdd9272bc8a662e74fc032e
domlockett <34702780+domlockett@users.noreply.github.com>
2018-08-15 12:08:11 -0500

    day 5 and hw initialized

--- homeworks/hw2/hw2_dl.py
+++ homeworks/hw2/hw2_dl.py
@@ -4,24 +4,37 @@
 ##   – Title # h3
 ##   – Published date # h4 class 'petition-attribution'
 ##   – Issues #div class = 'field field-name-field-petition-issues field-type-taxonomy-term-reference field-label-hidden tags'
+##   -- number of signatures #div class= 'signatures-progress-bar'
 from bs4 import BeautifulSoup
 import urllib2 
 import random
 import time
 import os
 import re
+import csv
 
 with open('hw2_dl.csv', 'wb') as f:
-	w = csv.DictWriter(f, fieldnames = ("Title", "Published date", "Issues"))
-	w.writeheader()
+    w = csv.DictWriter(f, fieldnames = ("Title", "Published date", "Issues"))
+    w.writeheader()
+    web_address='https://petitions.whitehouse.gov/petitions'
+    web_page = urllib2.urlopen(web_address)
+    all_html = BeautifulSoup(web_page.read())
+    all_html.prettify
+    all_topic = all_html.find_all("h3")[3:]
+    all_topics = [i.text for i in all_topics]
+    topic = []
+    extension=[]
+    for i in all_topics:
+        topic.append(i.encode('utf-8', 'ignore'))
+    dates = all_html.find_all('div', {'h4' : "petition-attribution"})
+   
+    ## collect website extension for the publish date
+    ext = all_html.find_all("a")
+    for i in ext:
+        extension.append(i['href'])
 
-web_address='https://petitions.whitehouse.gov/petitions '
-web_page = urllib2.urlopen(web_address)
 
-all_html = BeautifulSoup(web_page.read())
-all_topics = all_html.find_all("h3")[3:]
-all_topics = [i.text for i in all_topics]
-topic = []
-for i in all_topics:
-    topic.append(i.encode('utf-8', 'ignore'))
+    for i in all_topic:
+        extension.append = i.attrs["href"]
 
+all_topics
