b22579e33a67e4d3b4e1ef6d9b8faa18a814e860
domlockett <34702780+domlockett@users.noreply.github.com>
2018-08-21 10:07:17 -0500

    working hw3

--- homeworks/hw3/HW3_DL.py
+++ homeworks/hw3/HW3_DL.py
@@ -8,11 +8,12 @@ import imp
 import os
 import meetup.api
 import operator
-
+client.
 #define a space **Do i HAVE to do this outside function
     #in order to have it save into the local environment?
 group_info = {}
 names = []
+stlgroups = []
 size = []
 active = []
 orig = []
@@ -20,18 +21,18 @@ ids = []
 groups = []
 cwd = os.getcwd()
 cwd
-
+client.RateLimit
 ## basic setup 
 client = meetup.api.Client("c263c01e0c15052545d6537316b")
 meetup = imp.load_source('C:\Python27.14', 'C:\Python27.14\KEYS\meetup_KEY.py')
 api = meetup.client
 
-#identify a group based on some search criteria
-stlgroups = api.GetFindGroups({"zip" : "63116"})
+##identify a group based on some search criteria
+for p in range(200):
+    stlgroups.append( api.GetFindGroups({"zip" : "63116"})[p])
 len(stlgroups)
 
-#get the most popular group
-#x = []
+##get the the names and sizes of both groups
 def grab(groupList):
     for group in groupList:
         temp = group.urlname.encode('utf-8') # extract names to string
@@ -39,31 +40,36 @@ def grab(groupList):
         names.append(temp)
         size.append(temp2)
 
-#Make a dictionary
+##Make a dictionary
 group_info = dict(zip(names, size))
 
-# a couple quick tests to make sure dict aligns:
+###test to make sure dict aligns:
 names[0]
 size[0]
-group_info['Saint-Louis-Singles-Outdoor-Adventure-Group']# check
+group_info['Saint-Louis-Singles-Outdoor-Adventure-Group']
 
-names[33]
-size[33]
-group_info['Saint-Charles-Hiking-walking-Meetup']#check
+https://www.meetup.com/Saint-Louis-Singles-Outdoor-Adventure-Group # check
 
-#now lets do a quick sort and see whos most popular
-#we use the operator module so we can store the key with the sorted value
-sorted_gi = sorted(group_info.items(), key=operator.itemgetter(1))
-top_group = api.GetMembers({"group_urlname":  sorted_gi[-1][0]})
-ppl = top_group.__dict__["results"]
+##which group was most popular
+sorted_gi = sorted(group_info.items(), key=operator.itemgetter(1)) # use operator module to store key alongside value
+
+top_group = api.GetMembers({"group_urlname":  sorted_gi[-1][0]})#in ascending orders so grab 0 index-name from the last item in list
+
+##which member has most groups
+#**When grabbing member objects the max seems to be 200 members
+ppl = []
+for p in range(210):
+    ppl.extend(top_group.__dict__["results"][p])
+
+ppl = top_group.__dict__['results']
+
 len(ppl)
-group_info = dict(zip(names, size))
 members[0]
 
 def peeps(membList):
     for i in membList:
         ids.append(i['id'])
-        groups.append(temp2)
+        groups.append(api.GetGroups({"member_id" :  i['id']}).meta['total_count'])
 
-x =api.GetGroups({"member_id" : members[0]['id']}).results
-x['name']
+
+mem_info = dict(zip(ids, groups))
