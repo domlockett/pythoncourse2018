f7ec6785dfda29a7ac1a9fcf3bcf1af79ea6b36c
domlockett <34702780+domlockett@users.noreply.github.com>
2018-08-24 14:45:37 -0500

    hw5 __init__

---
+++ homeworks/hw5/hw5_dl.py
@@ -0,0 +1,33 @@
+class Node: 
+    def __init__(self, _value=None, _next=None):
+        self.value = _value 
+        self.next = _next 
+    def __str__(self):
+        return str(self.value)
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+
+    def length(self):
+        _length = 0
+        for i in node:
+            _length += 1
+
+    def addNode(self, new_value):
+        self.new_value = Node(new_value)
+
+
+    def addNodeAfter(self, new_value, after_node):
+
+    def addNodeBefore(self, new_value, before_node):
+
+    def removeNode(self, node_to_remove):
+
+    def removeNodesByValue(self, value):
+
+    def reverse(self):
+
+    def __str__(self):
+
+    def hasCycle(self):
