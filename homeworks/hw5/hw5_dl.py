b520a4527a0b0917449f3ed4878d5a55db8c6651
domlockett <34702780+domlockett@users.noreply.github.com>
2018-08-26 20:22:36 -0500

    add node after

--- homeworks/hw5/hw5_dl.py
+++ homeworks/hw5/hw5_dl.py
@@ -14,7 +14,7 @@ class LinkedList:
         return self.count #define length by adding a count to each function
 
 
-    def addNode(self, new_value):
+    def addNode(self, new_value): #This should be O(n+1) where n is the initial number of nodes plus the one added 
         _node = self.head#Loop through the linked list to the last element.
         while _node.next != None:
             _node = _node.next
@@ -22,10 +22,22 @@ class LinkedList:
         self.count += 1 #out counter of nodes
 
 
-    def addNodeAfter(self, new_value, after_node):
+    def addNodeAfter(self, new_value, after_node):#still O(n+1) for n=length of initial list.
+        new_value.next = after_node.next #Similar to previous code except we have t
+		after_node.next = new_value
+		self.count += 1
 
     def addNodeBefore(self, new_value, before_node):
-
+	    if before_node == self.head: #if the first value just add it
+		    new_value.next = self.head
+		    self.head = new_value
+	    else:
+		    _node = self.head 
+		    while _node.next != before_node:
+			    _node = _node.next
+		    _node.next = new_value		
+		    new_value.next = before_node
+	    self.count += 1
     def removeNode(self, node_to_remove):
 
     def removeNodesByValue(self, value):
